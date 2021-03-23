351c3be409ecbde0a43422c32e8d96543627dad6
24kevin-ai <81113762+24kevin-ai@users.noreply.github.com>
2021-03-23 01:19:49 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,10 @@
 
 firebase.initializeApp({
-   
-    });
+    apiKey: "AIzaSyCe_nmH6eo8xGZf59KIRyoo61kn70_iYKg",
+    authDomain: "autentifica43kus.firebaseapp.com",
+    projectId: "autentifica43kus",
+    storageBucket: "autentifica43kus.appspot.com",
+    messagingSenderId: "633947480746",
+    appId: "1:633947480746:web:523dc3ea40aeb8126226d3",
+    measurementId: "G-HQDYRME0DZ"
+  });
